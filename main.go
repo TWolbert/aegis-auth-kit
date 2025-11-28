2543f273f7755bc470079691268fbf7575d2d7d1
Teun Wolbert <teunw@goed.email>
2025-11-29 00:38:28 +0100

    Add tailwindcss

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("./templates/index.html", true)
+	})
+
+	app.Get("/message", func(c *fiber.Ctx) error {
+		return c.SendString("Test string hatsaa")
+	})
+
+	app.Get("/aegis-htmx.js", func(c *fiber.Ctx) error {
+		return c.SendFile("./js/htmx.min.js", true)
+	})
+
+	app.Get("/aegis-alpine.js", func(c *fiber.Ctx) error {
+		return c.SendFile("./js/alpine.min.js", true)
+	})
+
+	app.Get("/aegis-styles.css", func(c *fiber.Ctx) error {
+		return c.SendFile("./css/output.css", true)
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
